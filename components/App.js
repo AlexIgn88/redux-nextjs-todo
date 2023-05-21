4c46b6086d3f6cbbc68f1efbed2782d23832b530
unknown <aleksejignatov088@gmail.com>
2023-05-21 14:17:17 +0300

    первый коммит, подготовка к работе

---
+++ components/App.js
@@ -0,0 +1,14 @@
+import Counter from './Counter';
+import Height from './Height';
+import Width from './Width';
+
+
+export default function App() {
+  return <>
+    <hr />
+    <Counter name='A' />
+    <Counter name='B' />
+    <Height />
+    <Width />
+    <hr /></>;
+}
